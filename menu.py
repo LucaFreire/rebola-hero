27ddb6bfd02dd0f8bb43baa1da056d1d3b9a3b12
Hemerson <hemersonviolin@gmail.com>
2022-10-31 13:48:19 -0300

    conzertos increibles

--- menu.py
+++ menu.py
@@ -3,42 +3,46 @@ import pygame
 from pygame.locals import *
 from os import path
 from pygame import mixer
+import sys
+import math
+import random
+import time
 
-# Inicia jogo
+# # Inicia jogo
 pygame.init()
  
 # Define tamanho de tela
-window = pygame.display.set_mode((800, 600))
+tela = pygame.display.set_mode((800, 600))
  
 # Carrega imagens da tela principal em uma lista
-image_sprite = [pygame.image.load("./images_menu/00.png"),
-                pygame.image.load("./images_menu/01.png"),
-                pygame.image.load("./images_menu/02.png"),
-                pygame.image.load("./images_menu/03.png"),
-                pygame.image.load("./images_menu/04.png"),
-                pygame.image.load("./images_menu/05.png"),
-                pygame.image.load("./images_menu/06.png"),
-                pygame.image.load("./images_menu/07.png"),
-                pygame.image.load("./images_menu/08.png"),
-                pygame.image.load("./images_menu/09.png"),
-                pygame.image.load("./images_menu/10.png"),
-                pygame.image.load("./images_menu/11.png"),
-                pygame.image.load("./images_menu/12.png"),
-                pygame.image.load("./images_menu/13.png"),
-                pygame.image.load("./images_menu/14.png"),
-                pygame.image.load("./images_menu/15.png"),
-                pygame.image.load("./images_menu/16.png"),
-                pygame.image.load("./images_menu/17.png"),
-                pygame.image.load("./images_menu/18.png"),
-                pygame.image.load("./images_menu/19.png"),
-                pygame.image.load("./images_menu/20.png"),]
+image_sprite = [pygame.image.load("./images/00.png"),
+                pygame.image.load("./images/01.png"),
+                pygame.image.load("./images/02.png"),
+                pygame.image.load("./images/03.png"),
+                pygame.image.load("./images/04.png"),
+                pygame.image.load("./images/05.png"),
+                pygame.image.load("./images/06.png"),
+                pygame.image.load("./images/07.png"),
+                pygame.image.load("./images/08.png"),
+                pygame.image.load("./images/09.png"),
+                pygame.image.load("./images/10.png"),
+                pygame.image.load("./images/11.png"),
+                pygame.image.load("./images/12.png"),
+                pygame.image.load("./images/13.png"),
+                pygame.image.load("./images/14.png"),
+                pygame.image.load("./images/15.png"),
+                pygame.image.load("./images/16.png"),
+                pygame.image.load("./images/17.png"),
+                pygame.image.load("./images/18.png"),
+                pygame.image.load("./images/19.png"),
+                pygame.image.load("./images/20.png"),]
  
 # Cria objeto para ler tempo
 clock = pygame.time.Clock()
  
 # Seta algumas varíaveis globais para serem usadas posteriormente
 run = True
-value = 0
+frame_atual = 0
 menu = True
 pontos_atuais = 0
 fonte = pygame.font.Font('disposabledroid-bb.regular.ttf', 108)
@@ -50,15 +54,15 @@ def apareceTexto():
     global fontex
     global fontey
     global pontos_atuais
-    global window
+    global tela
     pontos_atuais+= 1
     texto = str(pontos_atuais)
 
     score = fonte.render(texto, 1, (255,255,255))
-    image = pygame.image.load("./images_menu/jesuis.png")
+    image = pygame.image.load("./images/jesuis.png")
 
-    window.blit(image, (0, 0))
-    window.blit(score, (0,0))
+    tela.blit(image, (0, 0))
+    tela.blit(score, (0,0))
     
     pygame.display.update()
     
@@ -70,25 +74,24 @@ def carrega_menu():
 
     while(menu):
         
-        global value
+        global frame_atual
 
         # Troca os frames a cada 13 milisegundos
         clock.tick(13)
 
         # Volta para primeira imagem, gerando o loop
-        if value >= len(image_sprite):
-            value = 0
+        if frame_atual >= len(image_sprite):
+            frame_atual = 0
 
         # Seta a imagem atual
-        image = image_sprite[value]
+        image = image_sprite[frame_atual]
     
         # Da o update na tela
-        window.blit(image, (0, 0))
+        tela.blit(image, (0, 0))
         pygame.display.update()
-    
-        window.fill((0, 0, 0))
 
-        value += 1
+
+        frame_atual += 1
 
         # Verifica eventos do pygame
         for event in pygame.event.get():
@@ -106,6 +109,7 @@ def carrega_menu():
                 if ((position[0] > 25 and position[0] < 220) and (position[1] > 373 and position[1] < 449)):
                     mixer.music.load(path.join("sounds","entrada_leaderboard.wav"))
                     mixer.music.play(1,0.0)
+
                     while pygame.mixer.music.get_busy() == True:
                         continue
 
@@ -126,12 +130,11 @@ def leaderboard():
     mixer.music.load(path.join("sounds","favela_bc.mp3"))
     mixer.music.play(-1)
     # Carrega imagem de fundo
-    image = pygame.image.load("./images_menu/jesuis.png")
+    image = pygame.image.load("./images/jesuis.png")
 
-    window.blit(image, (0, 0))
+    tela.blit(image, (0, 0))
     pygame.display.update()
     
-    window.fill((0, 0, 0))
 
 # Toca musica do menu, carrega ele e quando sair do menu inicia a tela de leaderboard
 mixer.music.load(path.join("sounds","favela_bc.mp3"))
@@ -139,17 +142,16 @@ mixer.music.play(-1)
 carrega_menu()
 leaderboard()
 
-# Roda eternamente verificações para realizar ações no jogo
-while run:
+# # Roda eternamente verificações para realizar ações no jogo
+# while run:
 
-    for event in pygame.event.get():
+#     for event in pygame.event.get():
         
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+#         if event.type == pygame.QUIT:
+#             pygame.quit()
+#             exit()
         
-        if (event.type == pygame.KEYDOWN):
+#         if (event.type == pygame.KEYDOWN):
             
-            apareceTexto()
-        
-    
+#             apareceTexto()
+
