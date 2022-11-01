ce3c470709c8113fae3d1c031bf62953574f5200
gabrielle <gabrielle.fialkoski@hotmail.com>
2022-11-01 16:31:33 -0300

    botão leaderboard > menu, arrumado botão play

--- presets.py
+++ presets.py
@@ -221,41 +221,44 @@ class leaderBoard(Status_Game):
         self.next = "leaderBoard"
     def Pygame_Evento(self, event):
 
-        level_1 = pygame.Rect(508, 380, 600, 450)
+        #area de previsao de botao
+        rect_bos = ButtonPlay.rect
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
-            if level_1.collidepoint(pos):
+            if rect_bos.collidepoint(pos):
                 select = mixer.Sound(path.join("sounds","entrada_leaderboard.wav"))
                 select.play()
-                time.sleep(.25)
                 self.next = "leaderBoard"
                 self.done = True
-
+            if ButtonBack.touche == False:
+                ButtonBack.touche = True
+                self.next = "main_menu"
+                self.done = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_q:
                 select = mixer.Sound(path.join("sounds","entrada_leaderboard.wav"))
                 select.play()
-                time.sleep(.25)
                 self.next = "main_menu"
                 self.done = True
 
+
     def update(self, screen, dt):
         self.draw(screen)
+        ButtonPlay.rect.center = (600, 500)
         ButtonGroups.update()
         ButtonGroups.draw(tela)
+
     def draw(self, screen):
         screen.fill((0,0,0))
         screen.blit(level1, (0, 0))
 
-class Botao(pygame.sprite.Sprite):
+class BotaoLeaderboard(pygame.sprite.Sprite):
     def __init__(self,*groups):
         super().__init__(*groups)
 
         self.image = pygame.image.load("images/20 (1).png").convert_alpha()
         self.image = pygame.transform.scale(self.image , [264, 96])
-        self.rect = pygame.Rect(264, 96,264, 96)
-        self.rect = self.image.get_rect()
-
+        self.rect = self.image.get_rect(center=(600,450))
         self.image1 = pygame.image.load("images/20 (1).png").convert_alpha()
         self.image2 = pygame.image.load("images/20 (2).png").convert_alpha()
 
@@ -267,16 +270,38 @@ class Botao(pygame.sprite.Sprite):
         if self.rect.collidepoint(self.MousePos):
             if self.mouse[0]:
                 self.touche = True
-                pygame.mouse.get_rel()
                 self.image = self.image2
             else:
                 self.touche = False
-                self.image = self.image1
+                self.image = self.image
+        pass
+class BotaoRetornar(pygame.sprite.Sprite):
+    def __init__(self,*groups):
+        super().__init__(*groups)
+
+        self.image = pygame.image.load("images/botao_retornar.png").convert_alpha()
+        self.image = pygame.transform.scale(self.image , [54,54])
+        self.rect = pygame.Rect(54,54,54,54)
+        self.rect = self.image.get_rect()
+        self.touche = True
+    def update(self):
+        self.mouse = pygame.mouse.get_pressed()
+        self.MousePos = pygame.mouse.get_pos()
+
+        if self.rect.collidepoint(self.MousePos):
+            if self.mouse[0]:
+                self.touche = True
+                pygame.mouse.get_rel()
+            else:
+                self.touche = False
         pass
 
 ButtonGroups = pygame.sprite.Group()
-Button1 = Botao(ButtonGroups)
-Button1.rect.center = (600, 500)
+ButtonPlay = BotaoLeaderboard(ButtonGroups)
+ButtonBack = BotaoRetornar(ButtonGroups)
+
+
+
 
 # Classe do Nível
 # class Level(Status_Game):
